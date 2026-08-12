2f7b5d779aae421ec07d7f2fd50f749466cf1c92
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-12 16:36:26 -0300

    Now, App.jsx have a state where we will temporarely insert our transactions and calculate the current balance basend on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Navbar from "./components/common/Navbar";
+
+const [transactions, setTransactions] = useState([
+  {id: "1", description: "Salário", amount: 3500, type: "income", category: "Trabalho", date: "2026-08-01"},
+  {id: "2", description: "Mercado", amount: 600, type: "expense", category: "Alimentação", date: "2026-08-05"}
+]);
+
+const currentBalance = transactions.reduce((acc, item) => {
+  return item.type === "income" ? acc + item.amount : acc - item.amount;
+}, 0);
 
 function App() {
   return(
     <div>
 
-      <Navbar />
+      <Navbar balance={currentBalance} />
     
     </div>
   );
