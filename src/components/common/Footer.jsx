bf4e17192b05a2d732d1ec11d8d9e202af7558b0
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-12 17:25:18 -0300

    Create a social media section

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,11 @@
 import { HardDrive } from "lucide-react"
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function Footer() {
     return(
         <footer className="border-t border-finance-border py-6 bg-finance-bg">
             <div className="max-w-7xl mx-auto flex justify-between items-center p-5">
+                
                 <div className="flex flex-col gap-2 font-semibold text-finance-text font-['Outfit']">
                     <span>FinanceControl</span>
                     <span>&copy; {new Date().getFullYear()} — Personal Finance Dashboard.</span>
@@ -14,7 +16,27 @@ function Footer() {
                     <span className="text-finance-text">Storage: <strong>Local Browser (Offline)</strong></span>
                 </div>
 
-                
+                <div className="flex items-center gap-4 text-finance-muted">
+                    <a
+                        href="https://github.com/daveesin"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-1 hover:text-finance-text transition-colors"
+                    >
+                        <FaGithub className="w-4 h-4" />
+                        <span>GitHub</span>
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/davi-silva-gonçalves-ba3121250"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-1 hover:text-finance-text transition-colors"
+                    >
+                        <FaLinkedin className="w-4 h-4" />
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+
             </div>
         </footer>
     );
