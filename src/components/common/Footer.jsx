b60bc3bd291c71c5bbd16095f4cc91bffdbf9f7c
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-12 17:08:01 -0300

    Start the creation of the Footer component

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,10 @@
 function Footer() {
     return(
-        <h1>Footer</h1>
+        <footer className="border-t border-finance-border py-6 bg-finance-bg">
+            <div className="max-w-7xl mx-auto flex justify-between items-center p-5">
+                <h1 className="text-finance-text font['Outfit'] text-2xl">Footer</h1>
+            </div>
+        </footer>
     );
 }
 
