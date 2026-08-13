79b53c07a363a345f1c70cb32ec6d9310a497925
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-13 13:56:29 -0300

    Pass the props to the components and use contains the boolean state because the modal is a page element, not a table element

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import TransactionsTable from "../components/transaction/TransactionsTable";
+import NewTransactionModal from "../components/transaction/NewTransactionModal";
 
 function Transactions() {
+
+    const [isTModalOpen, setIsTModalOpen] = useState(false);
+
     return(
         <div className="flex flex-col items-center justify-center gap-3">
 
-            <TransactionsTable />
+            <TransactionsTable onNTClick={setIsTModalOpen} />
+
+            <NewTransactionModal 
+                isTModalOpen={isTModalOpen}
+                onCloseNTModal={setIsTModalOpen}
+            />
 
         </div>
     );
